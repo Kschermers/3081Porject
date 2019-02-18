db4447cb99d23f4f9984eedda0b85726f982d501
KADIN SCHERMERS <scher528@umn.edu>
2019-02-18 13:59:12 -0600

    add(lab08): Adding Lab08 deliverables

---
+++ labs/lab07_style_doxy/src/obstacle.h
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Class Definitions
+ ******************************************************************************/
+/**
+ * @brief Obstacle generates obstacle objects for the robot to avoid
+ */
+class Obstacle {
+  public:
+     Obstacle() : radius_(10), position_(20.0,20.0) {}
+     /**
+     * @brief return the radius parameter of the Obstacle class
+     * radius can be any positive int.
+     *
+     * @return radius of obstacle
+     */
+     int get_radius() {return radius_;}
+     std::pair<double,double> get_pos() {return pos_;}
+  private:
+      int radius_;
+      std::pair<double, double> pos_;
+  }
