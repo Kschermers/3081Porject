30b9f3bb98793db41ad4f019004672ef22e3f02c
KADIN SCHERMERS <scher528@umn.edu>
2019-04-11 11:46:55 -0500

    -Added a extra line i forgot to reset starving_count_ if food is sensed

--- project/src/braitenberg_vehicle.cc
+++ project/src/braitenberg_vehicle.cc
@@ -77,6 +77,7 @@ void BraitenbergVehicle::SenseEntity(const ArenaEntity& entity) {
     closest_entity_ = &closest_light_entity_;
   } else if (entity.get_type() == kFood) {
     closest_entity_ = &closest_food_entity_;
+    starving_count_ = 0;
   } else if (entity.get_type() == kBraitenberg) {
     closest_entity_ = &closest_bv_entity_;
   }
